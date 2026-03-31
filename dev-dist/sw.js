28c0b9377cb96475e111448cbe7337d2cb184a4b
rushikesh-57 <rushikeshbabar57@gmail.com>
2026-03-31 12:33:11 +0530

    fix: update sign-in overlay position to fixed and increase z-index for improved visibility

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.8n73k7tq1h8"
+    "revision": "0.mq63ntubl9o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
